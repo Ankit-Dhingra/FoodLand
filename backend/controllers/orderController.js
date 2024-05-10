cd5237f63eb3aa3a835213c3b2b5a3bb7cdee57f
Ankit Dhingra <165490728+Ankit-Dhingra@users.noreply.github.com>
2024-05-10 21:46:15 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -38,8 +38,10 @@ const placeOrder = async (req, res) => {
         })
         
           const session = await stripe.checkout.sessions.create({
-            success_url: `http://localhost:5173/verify?success=true&orderId=${newOrder._id}`,
-            cancel_url: `http://localhost:5173/verify?success=false&orderId=${newOrder._id}`,
+            // success_url: `http://localhost:5173/verify?success=true&orderId=${newOrder._id}`,
+            // cancel_url: `http://localhost:5173/verify?success=false&orderId=${newOrder._id}`,
+            success_url: `https://foodland-live.netlify.app/verify?success=true&orderId=${newOrder._id}`, 
+            cancel_url: `https://foodland-live.netlify.app/verify?success=false&orderId=${newOrder._id}`,  
             line_items: line_items,
             mode: 'payment',
           });
